f12427ff4fad177e79e89e39a80e3a56f2df2b0c
shaky-sahil <sahilpk81@gmail.com>
2022-01-18 18:24:45 +0530

    restriction to unlogged users added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+    before_action :require_login
+    skip_before_action :require_login, only: [:index]
     def index
     @posts = Post.all
     end
@@ -8,4 +10,13 @@ class PostsController < ApplicationController
 
     def create
     end
+
+    private
+    def require_login
+        unless user_signed_in?
+            flash[:alert] = "You must be logged in to access this section"
+            redirect_to root_url # halts request cycle
+        end
+    end
+
 end
