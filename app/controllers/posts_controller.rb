a2db64c1b6bd1d956a427fea4235331843757cef
shaky-sahil <sahilpk81@gmail.com>
2022-01-18 21:04:01 +0530

    core functionlity complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,17 +6,30 @@ class PostsController < ApplicationController
     end
 
     def new
+        @post = Post.new
     end
 
     def create
+        @post = Post.new(post_params)
+        @post.user = current_user
+        if (@post.save)
+            redirect_to root_url
+        else
+            flash[:alert] = "oops! looks like you've made a mistake"
+            render :new
+        end
     end
 
     private
     def require_login
         unless user_signed_in?
-            flash[:alert] = "You must be logged in to access this section"
+            flash[:notice] = "You must be logged in to access this section"
             redirect_to root_url # halts request cycle
         end
     end
+
+    def post_params
+        params.require(:post).permit(:title, :body,)
+    end  
 
 end
